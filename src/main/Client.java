bee07bfebfcc4151f5066ea93ed48435b8ceae29
Kieran <15kpsherman@gmail.com>
2016-04-05 21:59:49 -0400

    Client and Server TCP Implementation
    Very basic Client/Server implementation.

---
+++ src/main/Client.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.net.Socket;
+
+public class Client {
+	
+	private static String hostName = "localhost";
+	private static int portNumber = 4444;
+	
+	public static void main(String [] args) throws Exception {
+		Socket socket = new Socket(hostName, portNumber);
+		
+		System.out.println("connection to "+socket.getInetAddress());
+	
+		socket.close();
+	}
+
+}
---
+++ src/main/Server.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	
+	private static int portNumber = 4444;
+
+	public static void main(String [] args) throws Exception {
+		ServerSocket serverSocket = new ServerSocket(portNumber);
+		Socket clientSocket = serverSocket.accept();
+			
+		System.out.println("connection from "+clientSocket.getInetAddress());
+		
+		serverSocket.close();
+	}
+	
+}
