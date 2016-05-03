02628fa2ddfa13aa160dc761de5612cf28deaceb
Kieran <15kpsherman@gmail.com>
2016-05-02 22:51:28 -0400

    more aesthetic

--- src/main/ui/components/NotificationPaneUI.java
+++ src/main/ui/components/NotificationPaneUI.java
@@ -38,7 +38,7 @@ public class NotificationPaneUI {
 		label.setFont(new Font("Avenir Next", Font.PLAIN, 12));
 		
 		JProgressBar progressBar = new JProgressBar();
-		progressBar.setBackground(new Color(255, 255, 255, 0));
+		progressBar.setBackground(new Color(120, 120, 180, 0));
 		
 		JPanel notification = new JPanel();
 		notification.setPreferredSize(new Dimension(Resources.WIDTH/6, 50));
@@ -59,6 +59,7 @@ public class NotificationPaneUI {
 		
 		JPanel panel = notification.getJPanel();
 		int disposeTime = notification.getDisposeTime();
+		int fps = 25;
 		
 		notificationSize += 1;
 		notifications.add(panel);
@@ -70,14 +71,14 @@ public class NotificationPaneUI {
 		Thread t = new Thread() {
 			public void run() {
 				JProgressBar progressBar = (JProgressBar)panel.getComponent(1);
-				progressBar.setMaximum(disposeTime/50);
+				progressBar.setMaximum(disposeTime/fps);
 				
-				for(int i = 0; i < disposeTime/50; i++) {
-					try { Thread.sleep(50); } catch (Exception e) {}
+				for(int i = 0; i < disposeTime/fps; i++) {
+					try { Thread.sleep(fps); } catch (Exception e) {}
 					progressBar.setValue(i);
 					
-					if((disposeTime/50)-i <= 255)
-						panel.setBackground(new Color(120, 120, 180, (disposeTime/50)-i));
+					if((disposeTime/fps)-i <= 100)
+						panel.setBackground(new Color(120, 120, 180, (disposeTime/fps)-i));
 				}
 				
 				notificationSize -= 1;
